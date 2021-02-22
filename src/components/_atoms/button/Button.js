79032b13220df6a204f4607291090b3c7aed808a
nnsncl <nicolas.mgta@gmail.com>
2021-02-22 21:33:12 +0100

    update component structure

--- src/components/_atoms/button/Button.js
+++ src/components/_atoms/button/Button.js
@@ -1,37 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './styles/button.css';
+import { Body } from './styles/index';
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export default function Button({ modifier, label, ...restProps }) {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
+    <Body modifiers={[modifier]}{ ...restProps } >
       {label}
-    </button>
+    </Body>
   );
 };
 
 Button.propTypes = {
   /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  modifier: PropTypes.oneOf(['small', 'medium', 'large']).isRequired,
   /**
    * Button contents
    */
@@ -43,8 +29,6 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
+  modifier: 'medium',
   onClick: undefined,
 };
