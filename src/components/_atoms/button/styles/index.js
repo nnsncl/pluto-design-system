8218abd8fe7d58bf76bdedc6c4240293c6bf7803
nnsncl <nicolas.mgta@gmail.com>
2021-02-22 20:57:51 +0100

    WIP / Button modifiers

---
+++ src/components/_atoms/button/styles/index.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+// import { applyStyleModifiers } from 'styled-components-modifiers'
+
+// const BUTTON_MODIFIERS = {
+
+//     small: () => `
+//         font-size: ${typeScale.small};
+//         padding: 1.3rem 1.9rem;
+//     `,
+
+//     medium: () => `
+//         font-size: ${typeScale.small};
+//         padding: 1.5rem 1.9rem;
+//     `,
+
+//     large: () => `
+//         font-size: ${typeScale.small};
+//         padding: 1.9rem 2.3rem;
+//     `,
+// }
+
+export const Button = styled.button``;
