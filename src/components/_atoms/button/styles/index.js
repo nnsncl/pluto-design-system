a57f876bc4b4f70f01ccb5c16e581bfad7582560
nnsncl <nicolas.mgta@gmail.com>
2021-02-22 21:32:57 +0100

    Set up button scales modifiers

--- src/components/_atoms/button/styles/index.js
+++ src/components/_atoms/button/styles/index.js
@@ -1,22 +1,50 @@
 import styled from 'styled-components';
-// import { applyStyleModifiers } from 'styled-components-modifiers'
+import { secondaryFont, typeScale, neutral } from '../../../../utils';
+import { applyStyleModifiers } from 'styled-components-modifiers'
 
-// const BUTTON_MODIFIERS = {
+const BUTTON_MODIFIERS = {
 
-//     small: () => `
-//         font-size: ${typeScale.small};
-//         padding: 1.3rem 1.9rem;
-//     `,
+    small: () => `
+        font-size: ${typeScale[10]};
+        padding: 1.3rem 1.9rem;
+        border-radius: .4rem;
+    `,
 
-//     medium: () => `
-//         font-size: ${typeScale.small};
-//         padding: 1.5rem 1.9rem;
-//     `,
+    medium: () => `
+        font-size: ${typeScale[20]};
+        padding: 1.9rem 2.3rem;
+        border-radius: .9rem;
+    `,
 
-//     large: () => `
-//         font-size: ${typeScale.small};
-//         padding: 1.9rem 2.3rem;
-//     `,
-// }
+    large: () => `
+        font-size: ${typeScale[20]};
+        padding: 1.9rem 3.6rem;
+        border-radius: .9rem;
+    `,
+}
 
-export const Button = styled.button``;
+export const Body = styled.button`
+    font-family: ${secondaryFont};
+    font-weight: 400;
+    line-height: 0;
+    letter-spacing: -0.03em;
+    text-decoration: none;
+
+    border: none;
+
+    color: ${neutral[60]};
+    background: ${neutral[100]};
+
+    transition: all ease-out .2s;
+    outline: none;
+    cursor: pointer;
+
+    :disabled {
+        background-color: ${neutral[80]};
+        border: none;
+        cursor: not-allowed;
+        opacity: 0.3;
+    }
+
+    ${applyStyleModifiers(BUTTON_MODIFIERS)}
+`;
