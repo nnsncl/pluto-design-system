dcb60a342ead81d0e8a1de55d2462b4569a3dd73
nnsncl <nicolas.mgta@gmail.com>
2021-02-22 20:40:13 +0100

    add typography utils

---
+++ src/utils/typography.js
@@ -0,0 +1,16 @@
+export const primaryFont = "'Nunito Sans', sans-serif";
+export const secondaryFont = "'Roboto', sans-serif";
+
+/* Scale: Minor Third 1.200 */
+export const typeScale = {
+    100: '5.16rem',
+    90: '4.3rem',
+    80: '3.583rem',
+    70: '2.986rem',
+    60: '2.488rem',
+    50: '2.074rem',
+    40: '1.728rem',
+    30: '1.44rem',
+    20: '1.2rem',
+    10: '1rem'
+}
