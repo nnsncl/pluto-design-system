3053b9ce2e8ce20b2fdcc3f0887aa9ff8192c007
nnsncl <nicolas.mgta@gmail.com>
2021-02-24 22:01:09 +0100

    use custom theme which use default sb dark theme colors

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Pluto Design System',
+  // brandUrl: 'https://example.com', TBD 
+  // brandImage: 'https://placehold.it/350x150',TBD
+});
