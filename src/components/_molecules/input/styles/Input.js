a7985ad1c0223cb7745df0a14633fdada607011a
nnsncl <nuniroland@gmail.com>
2021-03-17 21:03:41 +0100

    Edit styles

--- src/components/_molecules/input/styles/Input.js
+++ src/components/_molecules/input/styles/Input.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import { typeScale, neutral } from '../../../../utils';
+import { typeScale, neutral, primary } from '../../../../utils';
+
+import { lighten } from 'polished';
 
 export const Container = styled.div`
     display: flex;
@@ -9,6 +11,7 @@ export const Container = styled.div`
     p {
         margin: 0;
         line-height: 1em;
+        ${(props) => props.error === true && (`color: ${primary.red[100]};`)}
     }
 
     ${(props) => props.disabled === true && (`opacity: 0.3;`)}
@@ -17,16 +20,20 @@ export const Container = styled.div`
 export const Label = styled.label`
     font-size: ${typeScale[30]};
     font-weight: bold;
-    margin-bottom: 0.9rem;
+    margin-bottom: 1.3rem;
 `;
 
 export const Field = styled.input`
     font-size: ${typeScale[30]};
-    padding: 1.3rem 1.9rem;
-    border: .05rem solid transparent;
-    border-radius: 1.3rem;
-    margin-bottom: 1.3rem;
-    background: ${neutral[80]};
+    padding: 1.3em 1.9rem;
+    ${(props) => props.error === true
+        ? (`border: 2px solid ${primary.red[100]};`)
+        : (`border: 2px solid ${neutral[80]};`)
+    }
+    
+    border-radius: 1.6rem;
+    margin-bottom: 0.9rem;
+    background: ${neutral[50]};
     outline: none;
     transition: all ease-out .2s;
 
@@ -35,7 +42,18 @@ export const Field = styled.input`
     }
 
     :hover {
-        background: ${neutral[50]};
-        border: .05rem solid ${neutral[80]};;
+        ${(props) => props.error === true 
+            ? (` border: 2px solid ${primary.red[100]};
+                 box-shadow: 0px 0px 1px 3px ${lighten('0.3', primary.red[100])};`)
+            : (` border: 2px solid ${neutral[90]};
+                 box-shadow: 0px 0px 1px 3px ${lighten('0.8', neutral[100])};`)
+        }
+    }
+
+    :focus {
+        ${(props) => props.error === true
+        ? (`border: 2px solid ${primary.red[100]};`)
+        : (`border: 2px solid ${neutral[90]};`)
+    }
     }
 `;
