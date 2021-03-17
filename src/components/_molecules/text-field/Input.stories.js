50a46525fa10675c86800acdf2fe8b30649a5274
nnsncl <nuniroland@gmail.com>
2021-03-17 22:02:18 +0100

    Update story

--- src/components/_molecules/text-field/Input.stories.js
+++ src/components/_molecules/text-field/Input.stories.js
@@ -14,11 +14,47 @@ const Template = (args) => <TextField {...args} />;
 export const Simple = Template.bind({});
 Simple.args = {
   name: 'default-input',
-  type: 'url',
-  label: 'Text Field Label',
+  type: 'text',
+  label: 'Label',
+  placeholder: 'Placeholder',
+  defaultValue: '',
+  helper: '',
+  disabled: false,
+  error: false,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  name: 'default-input',
+  type: 'text',
+  label: 'Label',
+  placeholder: 'Placeholder',
+  defaultValue: '',
+  helper: '',
+  disabled: true,
+  error: false,
+};
+
+export const WithHelper = Template.bind({});
+WithHelper.args = {
+  name: 'default-input',
+  type: 'text',
+  label: 'Label',
   placeholder: 'Placeholder',
   defaultValue: '',
   helper: 'Helper Text',
   disabled: false,
   error: false,
+};
+
+export const onError = Template.bind({});
+onError.args = {
+  name: 'default-input',
+  type: 'text',
+  label: 'Label',
+  placeholder: 'Placeholder',
+  defaultValue: '',
+  helper: 'Helper Text',
+  disabled: false,
+  error: true,
 };
