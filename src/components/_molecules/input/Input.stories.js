d4fb93f44439ee7667014a16abc7b480a53aadaf
nnsncl <nuniroland@gmail.com>
2021-03-17 21:03:50 +0100

    Edit story

--- src/components/_molecules/input/Input.stories.js
+++ src/components/_molecules/input/Input.stories.js
@@ -14,9 +14,11 @@ const Template = (args) => <InputField {...args} />;
 export const Simple = Template.bind({});
 Simple.args = {
   name: 'default-input',
+  type: 'url',
   label: 'Text Field Label',
-  placeholder: '',
-  defaultValue: 'This is a default value',
+  placeholder: 'Placeholder',
+  defaultValue: '',
   helper: 'Helper Text',
   disabled: false,
+  error: false,
 };
