bbb666fd599302449149c3cdd61e71bb8aaec766
nnsncl <nuniroland@gmail.com>
2021-03-18 16:48:45 +0100

    Export native fields

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * from './components/_atoms/typography';
 export * from './components/_atoms/button';
+export * from './components/_atoms/native-field';
 export * from './components/_molecules/text-field';
