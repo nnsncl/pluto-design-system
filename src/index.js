08fef93c633c22917d69f8815c99eda342a119a1
nnsncl <nuniroland@gmail.com>
2021-03-18 18:26:28 +0100

    export component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export * from './components/_atoms/typography';
 export * from './components/_atoms/button';
 export * from './components/_atoms/native-field';
+export * from './components/_atoms/field';
 export * from './components/_molecules/text-field';
