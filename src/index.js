6a201363c42aff593da8b4836db8ccb2ce89a0fb
nnsncl <nuniroland@gmail.com>
2021-03-20 21:01:16 +0100

    Edit exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export * from './components/_atoms/typography';
-export * from './components/_atoms/button';
-export * from './components/_atoms/native-field';
-export * from './components/_atoms/field';
+export { default as Typography } from './components/_atoms/typography';
+export { default as Button } from './components/_atoms/button';
+export { default as NativeField } from './components/_atoms/native-field';
+export { default as Field } from './components/_atoms/field';
 
-export * from './components/_molecules/field-control';
-export * from './components/_molecules/native-field-control';
-export * from './components/_molecules/password-control';
+export { default as ControlledField } from './components/_molecules/field-control';
+export { default as ControlledNativeField } from './components/_molecules/native-field-control';
+export { default as ControlledPasswordField } from './components/_molecules/password-control';
