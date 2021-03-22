879d7c6ebcdfb593ffd3e51d19247fccc7c0e5a7
nnsncl <nuniroland@gmail.com>
2021-03-22 22:39:33 +0100

    As rendering control on buttons

--- src/components/_atoms/button/index.js
+++ src/components/_atoms/button/index.js
@@ -6,13 +6,14 @@ import { Body } from './styles/Button';
  * Buttons are used to initialize an action. Button labels express what action will
  * occur when the user interacts with it.
  */
-export default function Button({ modifier, label, ariaLabel, ...restProps }) {
+export default function Button({ modifier, label, ariaLabel, as, ...restProps }) {
   return (
     <Body
+      as={as}
       modifiers={[modifier]}
       type='button'
       aria-label={ariaLabel}
-      { ...restProps }
+      {...restProps}
     >
       <span>{label}</span>
     </Body>
@@ -20,18 +21,27 @@ export default function Button({ modifier, label, ariaLabel, ...restProps }) {
 };
 
 Button.propTypes = {
+
   /**
    * How large should the button be
    */
   modifier: PropTypes.oneOf(['small', 'medium', 'large']).isRequired,
+
+  /**
+  * Specify weither the Button should render as a link
+  */
+  as: PropTypes.oneOf(['a', 'Link']),
+
   /**
    * Button contents
    */
   label: PropTypes.string.isRequired,
+
   /**
    * aria-label have accessibility purpose, it must define the Component and its Action
    */
   ariaLabel: PropTypes.string.isRequired,
+
   /**
    * Optional click handler
    */
