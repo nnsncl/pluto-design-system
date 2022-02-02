ca736d30c63bf840ae614c5298532bdfe55c78d1
nnsncl <nuniroland@gmail.com>
2022-02-02 21:11:12 +0100

    reset project: prepare switch to ts

--- .storybook/theme.js
+++
@@ -1,9 +0,0 @@
-
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'dark',
-  brandTitle: 'Pluto Design System',
-  // brandUrl: 'https://example.com',
-  // brandImage: 'https://placehold.it/350x150'
-});
--- src/index.js
+++
@@ -1,5 +0,0 @@
-export * from './components/_atoms/typography';
-export * from './components/_atoms/button';
-
-export * from './components/_molecules/text-field';
-export * from './components/_molecules/password-control';
